10e8d8c1d6bbbda8299fa0fdafd9b4ef67dc4f9e
ramy282 <60857262+ramy282@users.noreply.github.com>
2024-06-08 22:58:20 +0300

    Update deployToOpenShift.groovy

--- vars/deployToOpenShift.groovy
+++ vars/deployToOpenShift.groovy
@@ -1,7 +1,7 @@
 def call(String openshiftCredentialsID, String openshiftClusterURL, String openshiftProject, String imageName) {
     withCredentials([string(credentialsId: openshiftCredentialsID, variable: 'OPENSHIFT_TOKEN')]) {
         sh """
-            oc login ${openshiftClusterURL} --token=${OPENSHIFT_TOKEN}
+            oc login ${openshiftClusterURL} --token=${OPENSHIFT_TOKEN} --insecure-skip-tls-verify=true
             oc project ${openshiftProject}
             oc new-app ${imageName}
             oc expose svc/${imageName}
